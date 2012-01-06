9b139de50e9ff4d18606af1ffdb01c7ffc3dcc75
Joel Holdbrooks <cjholdbrooks@gmail.com>
2012-01-06 01:07:31 -0800

    rename test for consistency

--- test/version.rb
+++ test/version.rb
@@ -1,7 +1,7 @@
 require 'test/unit'
 require File.expand_path('../../lib/vic.rb', __FILE__)
 
-class VicTest < Test::Unit::TestCase
+class VersionTest < Test::Unit::TestCase
   def test_version
     assert Vic::VERSION
   end
