5d05fc7ea8c49c5c763869884ee1d41736654fde
Joel Holdbrooks <cjholdbrooks@gmail.com>
2012-01-20 14:17:47 -0800

    make sure gemspec version reflects Vic::VERSION

--- test/version.rb
+++ test/version.rb
@@ -4,6 +4,11 @@ require 'test/unit'
 
 class VersionTest < Test::Unit::TestCase
   def test_version
-    assert_equal Vic::VERSION, '0.0.4'
+    assert_equal Vic::VERSION, '0.0.5'
+  end
+
+  def test_gemspec_version_matches_vic_version
+    vic_gemspec_version = File.read('vic.gemspec').match(/(\d\.\d\.\d)/)[0]
+    assert_equal Vic::VERSION, vic_gemspec_version
   end
 end
