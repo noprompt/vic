580ac0b3b22c6633dae6d6bdc5fd8ce144fa697d
Joel Holdbrooks <cjholdbrooks@gmail.com>
2012-04-23 06:41:46 -0700

    test for garbage values

--- test/color.rb
+++ test/color.rb
@@ -8,16 +8,16 @@ module Vic
       color = Color.new(0)
       assert_equal '#000000', color.to_gui
 
-      color = Color.new(15)
-      assert_equal '#ffffff', color.to_gui
+      color = Color.new(255)
+      assert_equal '#eeeeee', color.to_gui
     end
 
     def test_it_converts_a_color_to_cterm
       color = Color.new('#000000')
       assert_equal 0, color.to_cterm
 
-      color = Color.new('#ffffff')
-      assert_equal 15, color.to_cterm
+      color = Color.new('#eeeeee')
+      assert_equal 255, color.to_cterm
     end
 
     def test_it_correctly_identifies_color_types
@@ -37,6 +37,16 @@ module Vic
     def test_it_converts_to_standard_hex
       color = Color.new('afa')
       assert_equal '#aaffaa', color.send(:to_standard_hex)
+    end
+
+    def test_it_raises_an_error
+      color = Color.new('a')
+      assert_raises(Vic::Color::ColorError) { color.to_gui }
+      assert_raises(Vic::Color::ColorError) { color.to_cterm }
+
+      color = Color.new(-2)
+      assert_raises(Vic::Color::ColorError) { color.to_cterm }
+      assert_raises(Vic::Color::ColorError) { color.to_gui }
     end
   end # class TestColor
 end # module Vic
