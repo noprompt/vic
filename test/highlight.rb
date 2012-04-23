206e1c55aa9239a2156f99a587ff6bf2007f710d
Joel Holdbrooks <cjholdbrooks@gmail.com>
2012-04-22 17:50:20 -0700

    tests for highlight

---
+++ test/highlight.rb
@@ -0,0 +1,47 @@
+$:.unshift File.expand_path('../../lib', __FILE__)
+require 'minitest/autorun'
+require 'vic'
+
+module Vic
+  class TestHighlight < MiniTest::Unit::TestCase
+    def setup
+      @highlight = Vic::Highlight.new('Normal')
+    end
+
+    def test_it_updates_attributes
+      @highlight.guibg = "#333333"
+      @highlight.update_attributes!(guibg: "#000000")
+      assert_equal '#000000', @highlight.guibg
+    end
+
+    def test_it_forces
+      @highlight.force!
+      assert @highlight.force?
+    end
+
+    def test_it_sets_both_ctermbg_and_guibg
+      @highlight.fg = '000'
+      assert_equal '#000000', @highlight.guifg
+      assert_equal 0, @highlight.ctermfg
+    end
+
+    def test_it_sets_both_ctermfg_and_guifg
+      @highlight.fg = 'fff'
+      assert_equal '#ffffff', @highlight.guifg
+      assert_equal 15, @highlight.ctermfg
+    end
+
+    def test_it_sets_both_cterm_and_gui
+      @highlight.style = 'bold', 'italic'
+      assert_equal ['bold', 'italic'], @highlight.cterm
+      assert_equal ['bold', 'italic'], @highlight.gui
+    end
+
+    def test_it_raises_an_error
+      assert_raises(ArgumentError) { @highlight.ctermbg = 'lol' }
+      assert_raises(ArgumentError) { @highlight.ctermfg = 'omg' }
+      assert_raises(ArgumentError) { @highlight.guibg = 'hai' }
+      assert_raises(ArgumentError) { @highlight.guifg = 'bai' }
+    end
+  end # class TestHighlight
+end # module Vic
