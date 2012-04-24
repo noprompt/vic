4a618a0423473abed4f4a0ca996e00ede1272c59
Joel Holdbrooks <cjholdbrooks@gmail.com>
2012-04-23 20:33:12 -0700

    refactor

--- lib/vic/highlight.rb
+++ lib/vic/highlight.rb
@@ -1,5 +1,6 @@
 module Vic
   class Highlight
+
     attr_accessor :group
 
     def initialize(group, attributes={})
@@ -20,10 +21,15 @@ module Vic
     #
     # @return [Vic::Highlight]
     #   the updated highlight
+    #
+    # @api public
     def update_attributes!(attributes={})
       attributes.each_pair do |key, val|
         self.respond_to?(:"#{ key }=") && send(:"#{ key }=", val)
       end
+
+      # Returning self seems like the right thing to do when a method changes
+      # more than one property of an object.
       self
     end
 
@@ -80,11 +86,8 @@ module Vic
     #
     # @api public
     def bg=(color)
-      self.ctermbg = Color.new(color).to_cterm
-      self.guibg = Color.new(color).to_gui
-
-      # Returning self seems like the right thing to do when a method changes
-      # more than one property of an object.
+      color = Color.new(color)
+      @ctermbg, @guibg = color.to_cterm, color.to_gui
       self
     end
 
@@ -98,8 +101,8 @@ module Vic
     #
     # @api public
     def fg=(color)
-      self.ctermfg = Color.new(color).to_cterm
-      self.guifg = Color.new(color).to_gui
+      color = Color.new(color)
+      @ctermfg, @guifg = color.to_cterm, color.to_gui
       self
     end
 
@@ -113,8 +116,7 @@ module Vic
     #
     # @api public
     def style=(styles)
-      self.cterm = styles
-      self.gui = styles
+      @cterm, @gui = [select_styles(styles)] * 2
       self
     end
 
@@ -132,7 +134,7 @@ module Vic
     #
     # @api public
     def cterm=(*styles)
-      @cterm = styles.pop
+      @cterm = select_styles(styles)
     end
 
     # Set the ctermbg value
@@ -145,9 +147,7 @@ module Vic
     #
     # @api public
     def ctermbg=(color)
-      color = Color.new(color)
-      raise ArgumentError.new("invalid color #{ color }") unless color.cterm?
-      @ctermbg = color.to_cterm
+      @ctermbg = Color.new(color).to_cterm
     end
 
     # Set the ctermfg value
@@ -160,9 +160,7 @@ module Vic
     #
     # @api public
     def ctermfg=(color)
-      color = Color.new(color)
-      raise ArgumentError.new("invalid color #{ color }") unless color.cterm?
-      @ctermfg = color.to_cterm
+      @ctermfg = Color.new(color).to_cterm
     end
 
     attr_reader :gui, :guibg, :guifg
@@ -178,7 +176,7 @@ module Vic
     #
     # @api public
     def gui=(*styles)
-      @gui = styles.pop
+      @gui = select_styles(styles)
     end
 
     # Set the guibg value
@@ -191,9 +189,7 @@ module Vic
     #
     # @api public
     def guibg=(color)
-      color = Color.new(color)
-      raise ArgumentError.new("invalid color #{ color }") unless color.gui?
-      @guibg = color.to_gui
+      @guibg = Color.new(color).to_gui
     end
 
     # Set the guifg value
@@ -206,9 +202,25 @@ module Vic
     #
     # @api public
     def guifg=(color)
-      color = Color.new(color)
-      raise ArgumentError.new("invalid color #{ color }") unless color.gui?
-      @guifg = color.to_gui
+      @guifg = Color.new(color).to_gui
+    end
+
+    private
+
+    # Valid font styles *excluding* none
+    FONT_STYLE = /bold|(under(line|curl))|((re|in)verse)|italic|standout/o
+
+    # Selcect valid styles from a list
+    #
+    # @param [Mixed] styles
+    #   the list of styles
+    #
+    # @return [Array]
+    #   the list of valid styles
+    #
+    # @api private
+    def select_styles(*styles)
+      styles.flatten.select { |s| FONT_STYLE.match(s) }
     end
   end # class Highlight
 end # module Vic
